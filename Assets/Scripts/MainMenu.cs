8887aa563eec1f4d04d95f06a9e9e1b29e43ef8b
Abang Muhammad Hakim Bin Abang Kiprawi <abgmuhdhakim@gmail.com>
2022-05-26 22:04:34 +0800

    Update Canvas

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quit();
+    }
+
+    public void Quit()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
