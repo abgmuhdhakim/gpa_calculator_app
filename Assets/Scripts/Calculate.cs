2e2bdbe9e1a7fb832ab0a0260be41b8a17a8b6f9
Abang Muhammad Hakim Bin Abang Kiprawi <abgmuhdhakim@gmail.com>
2022-05-29 12:07:15 +0800

    Update Script not working

---
+++ Assets/Scripts/Calculate.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Calculate : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+        Debug.Log ("Hello");
+    }
+
+    /*public float Calc()
+    {
+        if (x == A || x == A+)
+        {
+            return 4.00;
+        }
+        else if (x == A-)
+        {
+            return 3.75;
+        }
+        else if (x == B+)
+        {
+            return 3.50;
+        }
+        else if (x == B)
+        {
+            return 3.25;
+        }
+        else if (x == B-)
+        {
+            return 3.00;
+        }
+        else if (x == C+)
+        {
+            return 2.75;
+        }
+        else if (x == C)
+        {
+            return 2.50;
+        }
+        else if (x == C-)
+        {
+            return 2.25;
+        }
+        else if (x == D+)
+        {
+            return 2.00;
+        }
+        else if (x == D)
+        {
+            return 1.00;
+        }
+        else
+        {
+            return 0.00;
+        }
+    }*/
+}
